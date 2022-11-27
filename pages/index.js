9170db02d0a182ef00aa9c25197034a8cba2fd0b
emeraldweb <chernov@biganto.com>
2022-11-27 23:25:48 +0400

    install next.js and check it

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Home</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main>Hello world</main>
+        </>
+    );
+}
